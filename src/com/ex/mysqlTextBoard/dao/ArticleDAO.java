b1aac745585f5509d64f13aaa5c003813b6054a8
k06060212 <k06060212@gmail.com>
2021-02-18 19:27:10 +0900

    connection 해제를 확실하게 하기 위한 안전장치 추가

--- src/com/ex/mysqlTextBoard/dao/ArticleDAO.java
+++ src/com/ex/mysqlTextBoard/dao/ArticleDAO.java
@@ -11,68 +11,71 @@ import com.ex.mysqlTextBoard.dto.Article;
 public class ArticleDAO {
 
 	public List<Article> getArticles() {
-
-		List<Article> articles = new ArrayList<>();
-		
-		String dbmsJdbcUrl = "jdbc:mysql://192.168.200.102:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
-		String dbmsLoginId = "gokuma";
-		String dbmsLoginPw = "gokuma123";
-		
-		// 기사 등록
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		// 연결 생성
-		// finally는 무조건 실행됨
 		Connection con = null;
+		List<Article> articles = new ArrayList<>();
+
 		try {
-			con = DriverManager.getConnection(dbmsJdbcUrl,dbmsLoginId,dbmsLoginPw);
-		}catch(SQLException e){
-			e.printStackTrace();
-		}finally {
-			System.out.println("여기는 항상 실행됨!!");
+
+
+			String dbmsJdbcUrl = "jdbc:mysql://192.168.200.102:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000&socketTimeout=60000";
+			String dbmsLoginId = "gokuma";
+			String dbmsLoginPw = "gokuma123";
+
+			// MySQL 드라이버 등록
 			try {
-				if(con != null) {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			// 연결 생성
+			// finally는 무조건 실행됨
+			try {
+				con = DriverManager.getConnection(dbmsJdbcUrl, dbmsLoginId, dbmsLoginPw);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} finally {
+
+			try {
+				if (con != null) {
 					con.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
-			}			
+			}
 		}
-		
-		
+
 		return articles;
 	}
-	
-	private List<Article> getFakeArticles(){
+
+	private List<Article> getFakeArticles() {
 		List<Article> articles = new ArrayList<>();
 		Article article;
 		// 첫번째 가짜 게시물 만들기
-				article = new Article();
-				article.id = 1;
-				article.regDate = "2021-02-18 18:22:22";
-				article.updateDate = "2021-02-18 18:22:22";
-				article.title = "제목1";
-				article.body = "내용1";
-				article.memberId = 1;
-				article.boardId = 1;
+		article = new Article();
+		article.id = 1;
+		article.regDate = "2021-02-18 18:22:22";
+		article.updateDate = "2021-02-18 18:22:22";
+		article.title = "제목1";
+		article.body = "내용1";
+		article.memberId = 1;
+		article.boardId = 1;
 
-				articles.add(article);
+		articles.add(article);
 
-				// 두번째 가짜 게시물 만들기
-				article = new Article();
-				article.id = 2;
-				article.regDate = "2021-02-18 18:22:23";
-				article.updateDate = "2021-02-18 18:22:23";
-				article.title = "제목1";
-				article.body = "내용1";
-				article.memberId = 1;
-				article.boardId = 1;
+		// 두번째 가짜 게시물 만들기
+		article = new Article();
+		article.id = 2;
+		article.regDate = "2021-02-18 18:22:23";
+		article.updateDate = "2021-02-18 18:22:23";
+		article.title = "제목1";
+		article.body = "내용1";
+		article.memberId = 1;
+		article.boardId = 1;
 
-				articles.add(article);
+		articles.add(article);
 		return articles;
 	}
 
